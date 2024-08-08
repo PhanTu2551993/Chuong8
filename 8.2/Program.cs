2a941c4b2a82266099a8bab2d8d09f6799b5310d
PhanTu2551993 <phanvantu25051993@gmail.com>
2024-08-08 16:45:06 +0700

    Add project files.

---
+++ 8.2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _8._2
+{
+
+    class Balance
+    {
+        private double _balanceAmount;
+
+        public double BalanceAmount
+        {
+
+            // Lỗi phương thức get trả về value là sai. 
+            //get
+            //{
+            //    return value;
+            //}
+
+            // Sửa lỗi :
+            get
+            {
+                return _balanceAmount;
+            }
+            set
+            {
+                _balanceAmount = value;
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Balance objBal = new Balance();
+                objBal.BalanceAmount = 1005.50;
+                Console.WriteLine(objBal.BalanceAmount);
+            }
+        }
+    }
+}
---
+++ Chuong8/Program.cs
@@ -0,0 +1,47 @@
+﻿using Chuong8;
+
+namespace Chuong8
+{
+    class EmployeeDetails
+    {
+        private string _empName;
+        private int _empId;
+        public string EmpName
+        {
+
+            //Lỗi get, set không có ()
+            //    get() 
+            //     { 
+            //     return _empName; 
+            //     }
+            //        set()
+            //        {
+            //            _empName = value;
+            //        }
+
+            //sửa 
+
+            get
+             { 
+             return _empName; 
+             }
+            set
+            {
+                _empName = value;
+            }
+
+        }
+     public void SetId(int val)
+        {
+            _empId = val;
+        }
+        static void Main(string[] args)
+        {
+            EmployeeDetails objDetails = new EmployeeDetails();
+            objDetails.EmpName = "Bill";
+            objDetails.SetId(10);
+            Console.WriteLine("Employee Name:" + objDetails.EmpName);
+            Console.WriteLine("Employee Id:" + objDetails._empId);
+        } 
+    }
+}
